1db8e19de535f78149a40d1c82af23086f427bbb
albertojr <alberto.crato13@gmail.com>
2019-02-21 00:09:07 -0300

    Atulização do exercicio 63 e 64

--- ex063.py
+++ ex063.py
@@ -1,12 +1,13 @@
-n = int(input('Digite o valor inteiro: '))
-fib = 1
-i = 1
-aux = 0
-while i <= n:
-    aux = fib
-    fib = aux-i
-    print(fib)
-    i += 1
-
-
-
+n = int(input('Quantos termos você quer mostrar? '))
+print('SEQUÊNCIA DE FIBONACCI')
+t1=0
+t2 = 1
+print('{} -> {}'.format(t1, t2), end='')
+cont = 3
+while cont <= n:
+    t3 =t1+t2
+    print('-> {}'.format(t3),end='')
+    t1 = t2
+    t2 = t3
+    cont +=1
+print('\nfim')
--- ex064.py
+++ ex064.py
@@ -1,9 +1,9 @@
-num = int(input('Digite um numero: '))
+num = int(input('Digite um numero[Digite 99 para parar]: '))
 contNum = 0
 totNum = 0
 while num != 99:
     totNum += num
-    num = int(input('Digite um numero: '))
+    num = int(input('Digite um numero[Digite 99 para parar]: '))
     contNum += 1
 print('Foram digitados {} Números'.format(contNum))
 print('Somátorio dos números digitados {}'.format(totNum))
